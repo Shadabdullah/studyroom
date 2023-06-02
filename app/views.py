55e1a000320e4c4baa24acd461cc2b619f592b20
Shadabdullah <shadabdullah101@gmail.com>
2023-06-02 11:33:55 +0530

    Restarting

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from .models import Room
+
+from .forms import RoomForm
 # Create your views here.
 
 
@@ -14,3 +16,14 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {"room":room}
     return render(request, 'room.html' ,context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form':form}
+    return render(request , 'room_form.html', context )
