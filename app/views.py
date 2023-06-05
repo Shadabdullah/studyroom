c5f1c2c7f4bd6bf8db3040b2d8fc51dcd59ca105
Shadabdullah <shadabdullah101@gmail.com>
2023-06-05 13:45:37 +0530

    Browse Room

--- app/views.py
+++ app/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room ,Topic
+from .models import Room, Topic
 from .forms import RoomForm
+from django.db.models import Q
 # Create your views here.
 
 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+  
+    rooms = Room.objects.filter( 
+        Q(topic__name__icontains=q)| 
+        Q(name__icontains=q) | 
+        Q(description__icontains=q))
+    room_count = rooms.count()
     topic = Topic.objects.all()
-    context = {'rooms': rooms,'topic':topic}
+    context = {'rooms': rooms, 'topic': topic, 'room_count':room_count}
     return render(request, 'home.html', context)
 
 
